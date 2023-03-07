8567bf3340b0ad0d1964ebb8367190667e01e3c9
Ernest Mamba <fistozz.mamba@gmail.com>
2023-03-07 23:36:32 +0200

    re-adding zero division error handlers

--- test_calculator.py
+++ test_calculator.py
@@ -17,7 +17,15 @@ class TestCalculator(unittest.TestCase):
         cal = Calculator(8, 3)
         self.assertEqual(cal.subtract(), 5)
 
-
+    def test_modulus_raise_zero_division_error(self):
+        cal = Calculator(8, 0)
+        self.assertRaises(ZeroDivisionError, cal.modulus)
+    
+    def test_division_raises_zero_divion_error(self):
+        cal = Calculator(8, 0)
+        with self.assertRaises(ZeroDivisionError):
+            cal.divide()
+    
     def test_multiplication(self):
         cal = Calculator(-2, 3)
         self.assertEqual(cal.multiply(), -6)
